55845498ff2d705898a862b3d2b839641251ba31
Alanis Araujo <alanisaraujocosta@gmail.com>
2022-12-15 16:56:13 -0300

    CalculadoraReact

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+import './App.css';
+import Calculadora from './components/Calculadora';
+
+export default function App() {
+  return (
+    <div className='wrapper'>
+        <Calculadora/>
+    </div>
+
+
+  );
+}
